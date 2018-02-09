14782d6408fe5074dc92caf2f2a43d31e2eea07a
Elif <easlioymak@gmail.com>
2018-02-09 19:28:55 +0300

    ı created the main tab

---
+++ app/src/main/java/com/example/user/keepingmeontrack/fragments/FitnessMainFragment.java
@@ -0,0 +1,24 @@
+package com.example.user.keepingmeontrack.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.user.keepingmeontrack.R;
+
+/**
+ * Created by User on 06.02.2018.
+ */
+
+public class FitnessMainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_main_fitness , container, false);
+       // ((SwipeActivity)getActivity()).updateStatusBarColor("#ffb829");
+
+
+        return rootView;}
+}
