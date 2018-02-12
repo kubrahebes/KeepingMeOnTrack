a2020152f2a5a1c4d5db26c88a6638d893f88e97
MahmoudMuhammed92 <mahmoud.roxy@gmail.com>
2018-02-12 19:38:23 +0300

    Created animations when swiping pages

--- app/src/main/java/com/example/user/keepingmeontrack/SplashActivity.java
+++ app/src/main/java/com/example/user/keepingmeontrack/SplashActivity.java
@@ -6,6 +6,8 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.user.keepingmeontrack.swipeanimation.IntroActivity;
+
 /**
  * Created by Mahmoud on 1/31/2018.
  */
@@ -20,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, SwipeActivity.class));
+                startActivity(new Intent(SplashActivity.this, IntroActivity.class));
             }
         },3000);
     }
