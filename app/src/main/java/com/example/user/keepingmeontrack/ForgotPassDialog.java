10f658f34c6f56ba8f2891c4f971894805fb5abc
kubra <kubra.hebes@gmail.com>
2018-02-16 17:56:32 +0200

    new code

--- app/src/main/java/com/example/user/keepingmeontrack/ForgotPassDialog.java
+++ app/src/main/java/com/example/user/keepingmeontrack/ForgotPassDialog.java
@@ -20,102 +20,71 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Unbinder;
 
 public class ForgotPassDialog extends DialogFragment {
 
-    @BindView(R.id.forgot_logo)
-    ImageView forgotLogo;
-    @BindView(R.id.forgot_text)
-    TextView forgotText;
-    @BindView(R.id.forgot_username)
-    EditText forgotUsername;
-    @BindView(R.id.forgot_button)
-    Button forgotButton;
-    Unbinder unbinder;
 
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        LayoutInflater inflater = getActivity().getLayoutInflater();
+            LayoutInflater inflater = getActivity().getLayoutInflater();
 
-        final View addView = inflater.inflate(R.layout.forgotpass_layout, null);
+            final View addView = inflater.inflate(R.layout.forgotpass_layout, null);
 
-        final Dialog addDialog = builder.setView(addView)
-                .create();
-        addDialog.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(DialogInterface dialogInterface) {
+            final Dialog addDialog = builder.setView(addView)
+                    .create();
+            addDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                @Override
+                public void onShow(DialogInterface dialogInterface) {
 
-                Button positiveButton = ((AlertDialog) dialogInterface).getButton(AlertDialog.BUTTON_POSITIVE);
-                Button forgotBtn = addDialog.findViewById(R.id.forgot_button);
-                forgotBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        FirebaseAuth auth = FirebaseAuth.getInstance();
-                        final EditText forgotUname = addView.findViewById(R.id.forgot_username);
-                        String emailAddress = forgotUname.getText().toString();
-                        if (emailAddress.isEmpty()) {
-                            Toast.makeText(getActivity(), "Enter the your email adress", Toast.LENGTH_SHORT).show();
-                        } else {
+                    Button positiveButton = ((AlertDialog) dialogInterface).getButton(AlertDialog.BUTTON_POSITIVE);
+                    Button forgotBtn = addDialog.findViewById(R.id.forgot_button);
+                    forgotBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            FirebaseAuth auth = FirebaseAuth.getInstance();
+                            final EditText forgotUname = addView.findViewById(R.id.forgot_username);
+                            String emailAddress = forgotUname.getText().toString();
+                            if (emailAddress.isEmpty()) {
+                                Toast.makeText(getActivity(), "Enter the your email adress", Toast.LENGTH_SHORT).show();
+                            } else {
 
 
-                            auth.sendPasswordResetEmail(emailAddress)
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
-                                                Toast.makeText(getActivity(), "sucsess", Toast.LENGTH_SHORT).show();
-                                                forgotUname.setText("");
-                                            } else {
-                                                Toast.makeText(getActivity(), "fail", Toast.LENGTH_SHORT).show();
+                                auth.sendPasswordResetEmail(emailAddress)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(getActivity(), "sucsess", Toast.LENGTH_SHORT).show();
+                                                    forgotUname.setText("");
+                                                } else {
+                                                    Toast.makeText(getActivity(), "fail", Toast.LENGTH_SHORT).show();
+                                                }
                                             }
-                                        }
-                                    });
+                                        });
+                            }
                         }
-                    }
-                });
-                positiveButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
+                    });
+                    positiveButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
 
-                        EditText forgotUname = addView.findViewById(R.id.forgot_username);
+                            EditText forgotUname = addView.findViewById(R.id.forgot_username);
 
-                        String forgotName = forgotUname.getText().toString().trim();
+                            String forgotName = forgotUname.getText().toString().trim();
 
-                        if (TextUtils.isEmpty(forgotName)) {
-                            Toast.makeText(getActivity(), "Failed to send", Toast.LENGTH_SHORT).show();
-                        } else if (!forgotName.matches("[a-zA-Z0-9._-]+@[a-z]+.[a-z]+")) {
-                            Toast.makeText(getActivity(), "Invalid email address!", Toast.LENGTH_SHORT).show();
+                            if (TextUtils.isEmpty(forgotName)) {
+                                Toast.makeText(getActivity(), "Failed to send", Toast.LENGTH_SHORT).show();
+                            } else if (!forgotName.matches("[a-zA-Z0-9._-]+@[a-z]+.[a-z]+")) {
+                                Toast.makeText(getActivity(), "Invalid email address!", Toast.LENGTH_SHORT).show();
+                            }
+
                         }
+                    });
+                }
+            });
 
-                    }
-                });
-            }
-        });
-
-        return addDialog;
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // TODO: inflate a fragment view
-        View rootView = super.onCreateView(inflater, container, savedInstanceState);
-        unbinder = ButterKnife.bind(this, rootView);
-        return rootView;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder.unbind();
-    }
-
-    @OnClick(R.id.forgot_button)
-    public void onViewClicked() {
-    }
+            return addDialog;
+        }
 }
