577c4c0cfacd0941e0a4da55feefe7c3146eb708
Elif <easlioymak@gmail.com>
2018-02-16 21:33:58 +0300

    I added detail page for finance

---
+++ app/src/main/java/com/example/user/keepingmeontrack/FinanceGoalDetail.java
@@ -0,0 +1,62 @@
+package com.example.user.keepingmeontrack;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+/**
+ * Created by elifasli on 16.02.2018.
+ */
+
+public class FinanceGoalDetail extends AppCompatActivity {
+    PieChart pieChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.finance_goal_detail);
+
+        pieChart = (PieChart) findViewById(R.id.piechart);
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(0,0,0,0);
+
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        //içi dolu istersen hole false yap
+        pieChart.setDrawHoleEnabled(false);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(61f);
+        //şeffaflık kalksın diye transparanı 30f yap.
+
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+        yValues.add(new PieEntry(34f,"Hire"));
+        yValues.add(new PieEntry(36f,"Foods"));
+        yValues.add(new PieEntry(30f,"Technology"));
+
+        pieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic);
+
+        PieDataSet dataSet = new PieDataSet(yValues,"Finance");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
+        PieData data =new PieData((dataSet));
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.YELLOW);
+
+        pieChart.setData(data);
+
+
+    }
+}
